8661608e346bc6e10950cee6913bd1b833f92b42
Jeremy Kuhne <jkuhne@microsoft.com>
2015-09-16 11:31:19 -0700

    Move to VS14 text templating packages.

--- src/xTask/Utility/AutoGeneratedConstants.cs
+++ src/xTask/Utility/AutoGeneratedConstants.cs
@@ -20,22 +20,22 @@ namespace XTask.Utility
             /// <summary>
             /// Current month 1..12
             /// </summary>
-            public const int CurrentMonth = 8;
+            public const int CurrentMonth = 9;
 
             /// <summary>
             /// Current day of the month 1..31
             /// </summary>
-            public const int CurrentDayOfMonth =  31;
+            public const int CurrentDayOfMonth =  16;
 
             /// <summary>
             /// Current hour from 0..23
             /// </summary>
-            public const int CurrentHour = 16;
+            public const int CurrentHour = 11;
 
             /// <summary>
             /// Current hour from 0..59
             /// </summary>
-            public const int CurrentMinute = 41;
+            public const int CurrentMinute = 29;
         }
 
         public static class Strings
@@ -48,42 +48,42 @@ namespace XTask.Utility
             /// <summary>
             /// Current month 1..12
             /// </summary>
-            public const string CurrentMonth = "8";
+            public const string CurrentMonth = "9";
 
             /// <summary>
             /// Current month 01..12
             /// </summary>
-            public const string CurrentTwoDigitMonth = "08";
+            public const string CurrentTwoDigitMonth = "09";
 
             /// <summary>
             /// Current day of the month 1..31
             /// </summary>
-            public const string CurrentDayOfMonth =  "31";
+            public const string CurrentDayOfMonth =  "16";
 
             /// <summary>
             /// Current two digit day of the month 01..31
             /// </summary>
-            public const string CurrentTwoDigitDayOfMonth =  "31";
+            public const string CurrentTwoDigitDayOfMonth =  "16";
 
             /// <summary>
             /// Current hour from 0..23
             /// </summary>
-            public const string CurrentHour = "16";
+            public const string CurrentHour = "11";
 
             /// <summary>
             /// Current two digit hour from 00..23
             /// </summary>
-            public const string CurrentTwoDigitHour = "16";
+            public const string CurrentTwoDigitHour = "11";
 
             /// <summary>
             /// Current hour from 0..59
             /// </summary>
-            public const string CurrentMinute = "41";
+            public const string CurrentMinute = "29";
 
             /// <summary>
             /// Current two digit hour from 00..59
             /// </summary>
-            public const string CurrentTwoDigitMinute = "41";
+            public const string CurrentTwoDigitMinute = "29";
         }
     }
 }
