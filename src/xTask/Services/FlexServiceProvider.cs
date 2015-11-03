89057e03c192a625244068e857011b993197e897
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2015-11-03 14:20:53 -0800

    Add missing FlexServiceProvider class.

---
+++ src/xTask/Services/FlexServiceProvider.cs
@@ -0,0 +1,43 @@
+﻿// ----------------------
+//    xTask Framework
+// ----------------------
+
+// Copyright (c) Jeremy W. Kuhne. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace XTask.Services
+{
+    using Systems.Configuration;
+    using Systems.Configuration.Concrete;
+    using Systems.Console;
+    using Systems.Console.Concrete;
+    using Systems.File;
+    using Systems.File.Concrete;
+    using Systems.File.Concrete.Flex;
+
+    /// <summary>
+    /// Flexible services provider
+    /// </summary>
+    public static class FlexServiceProvider
+    {
+        private static SimpleServiceProvider concreteServices;
+
+        static FlexServiceProvider()
+        {
+            concreteServices = new SimpleServiceProvider();
+            var extendedFileService = new ExtendedFileService();
+            concreteServices.AddService<IExtendedFileService>(extendedFileService);
+            concreteServices.AddService<IFileService>(new FileService(extendedFileService));
+            concreteServices.AddService<IConsoleService>(new ConcreteConsoleService());
+            concreteServices.AddService<IConfigurationManager>(new ConfigurationManager());
+        }
+
+        public static ITypedServiceProvider Services
+        {
+            get
+            {
+                return FlexServiceProvider.concreteServices;
+            }
+        }
+    }
+}
