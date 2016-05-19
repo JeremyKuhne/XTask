b6145a1d2df2bfe5c5d8076bdfd216fcf4c4e538
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2016-05-18 18:50:49 -0700

    Remove crypto wraps

--- src/xTask/Utility/AutoGeneratedConstants.cs
+++ src/xTask/Utility/AutoGeneratedConstants.cs
@@ -30,12 +30,12 @@ namespace XTask.Utility
             /// <summary>
             /// Current hour from 0..23
             /// </summary>
-            public const int CurrentHour = 16;
+            public const int CurrentHour = 18;
 
             /// <summary>
             /// Current hour from 0..59
             /// </summary>
-            public const int CurrentMinute = 15;
+            public const int CurrentMinute = 48;
         }
 
         public static class Strings
@@ -68,22 +68,22 @@ namespace XTask.Utility
             /// <summary>
             /// Current hour from 0..23
             /// </summary>
-            public const string CurrentHour = "16";
+            public const string CurrentHour = "18";
 
             /// <summary>
             /// Current two digit hour from 00..23
             /// </summary>
-            public const string CurrentTwoDigitHour = "16";
+            public const string CurrentTwoDigitHour = "18";
 
             /// <summary>
             /// Current hour from 0..59
             /// </summary>
-            public const string CurrentMinute = "15";
+            public const string CurrentMinute = "48";
 
             /// <summary>
             /// Current two digit hour from 00..59
             /// </summary>
-            public const string CurrentTwoDigitMinute = "15";
+            public const string CurrentTwoDigitMinute = "48";
         }
     }
 }
